7c54074a0855cf5bc347e74ab7776b99d14c35b3
jonny <nfernal13@outlook.com>
2017-03-18 15:29:04 -0700

    added task controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'pages#home'
 
+  devise_for :users
+  resources :tasks
+  root 'pages#home'
   get 'about' => 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

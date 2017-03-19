f6c72b72d9f6ac25090c999eb6ee95d87ccdfca5
jonny <nfernal13@outlook.com>
2017-03-18 19:18:17 -0700

    added edit and update actions, created edit view file

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,11 @@
 class TasksController < ApplicationController
+
   def new
     @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
   end
 
   def create
@@ -17,8 +22,20 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(tasks_params)
+      flash[:notice] = "Task was successfully updated"
+      redirect_to task_path(@task)
+    else
+      render 'edit'
+    end
+  end
+
   private
+
   def tasks_params
     params.require(:task).permit(:content)
   end
+
 end

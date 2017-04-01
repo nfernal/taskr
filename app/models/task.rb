fa6835a00d02b2d0d96e0a4a1a530a19af320b12
jonny <nfernal13@outlook.com>
2017-04-01 10:43:22 -0700

    added auto_html gem

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,10 +4,9 @@ class Task < ApplicationRecord
   belongs_to :user
 
   auto_html_for :content do
-
     html_escape
     image
-    # youtube
+    youtube(width: '100%', height: 250)
     link target: '_blank', rel: 'nofollow'
     simple_format
   end
